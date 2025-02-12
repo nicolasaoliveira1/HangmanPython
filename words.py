1b785448461d5d550c92bbde5ef25a4ed55c05c9
Nicolas <nicolasimbe2006@gmail.com>
2025-02-12 15:55:52 -0300

    hangman commit

---
+++ words.py
@@ -0,0 +1,3 @@
+#words for hangman game - fruits
+
+words = ("apple","orange","banana","strawberry","coconut","watermelon","mango","jackfruit","grapes","pineapple","avocado","cherry","kiwi","lemon","tangerine","raspberry","blueberry")
